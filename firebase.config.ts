5a287d1cb22c75c35aab7e5682500eb134dacaaa
solexki <solomonobafemi@gmail.com>
2025-08-16 14:33:06 +0100

    Remove .firebase directory from repo and public directory

--- firebase.config.ts
+++
@@ -1,8 +0,0 @@
-// firebase.config.js
-module.exports = {
-  hosting: {
-    site: "solob-portfolio", // optional
-    public: "public",
-    ignore: ["firebase.json", "/.", "/node_modules/*"],
-  },
-};
