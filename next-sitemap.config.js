e2c99fa8bca8f0ace955a8b26831446dc1925ad4
solexki <solomonobafemi@gmail.com>
2025-08-16 23:51:50 +0100

    Setting up SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: "https://solob.dev",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,14 +13,48 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  title: "Solomon Portfolio",
-  description: "Solomon's portfolio showcasing projects and skills.",
-  keywords: "portfolio, web development, projects, skills",
-  authors: [{ name: "Solomon" }],
+  title: "Solomon Obafemi | Full-Stack Developer",
+  description:
+    "Solomon Obafemi's portfolio showcasing projects, skills, and experience in building dynamic and scalable web applications with React, React Native, Node.js, Firebase, etc.",
+  keywords: [
+    "Full stack developer",
+    "React",
+    "Node.js",
+    "Firebase",
+    "Portfolio",
+    "Solomon Obafemi",
+  ],
+  authors: [{ name: "Solomon Obafemi", url: "https://solob.dev" }],
+  openGraph: {
+    title: "Solomon Obafemi | Full-Stack Developer",
+    description:
+      "Explore Solomon's portfolio: scalable web apps, innovative projects, and modern tech stacks.",
+    url: "https://solob.dev",
+    siteName: "Solomon Portfolio",
+    images: [
+      {
+        url: "https://solob.dev/preview.png", // (1200x630px)
+        width: 1200,
+        height: 630,
+        alt: "Solomon Obafemi Portfolio Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Solomon Obafemi | Full-Stack Developer",
+    description:
+      "Building dynamic and scalable applications with React, React Native, Node.js, Firebase, etc.",
+    creator: "@solexki",
+    images: ["https://solob.dev/preview.png"],
+  },
   icons: {
     icon: "/fav/favicon.ico",
     apple: "/fav/apple-touch-icon.png",
   },
+  metadataBase: new URL("https://solob.dev"),
 };
 
 export default function RootLayout({
@@ -30,7 +64,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        {children}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Person",
+              name: "Solomon Obafemi",
+              url: "https://solob.dev",
+              jobTitle: "Full-Stack Developer",
+              sameAs: [
+                "https://github.com/solexki",
+                "https://www.linkedin.com/in/solexki/",
+                "https://twitter.com/solexki",
+              ],
+            }),
+          }}
+        />
+      </body>
     </html>
   );
 }
